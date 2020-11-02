f8554f5e7cfe034997fbb532fa79583ee42d3892
Luis Hernandez <lfhernan@fiu.edu>
2020-11-02 01:21:32 -0500

    Updated auth-video

--- src/components/auth-video/auth-video.component.ts
+++ src/components/auth-video/auth-video.component.ts
@@ -1,17 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Pipe({
 	name: 'authVideo'
 })
 export class AuthVideoComponent implements PipeTransform {
 	constructor(
-		private http: HttpClient, 
+		private http: HttpClient,
+		private auth: AuthService
 	) { }
 
 	async transform(src: string): Promise<string> {
+		const token = this.auth.getAccessTokenSilently();
+		const headers = new HttpHeaders({'Authorization': `Bearer ${token}`});
 		try {
-			const videoBlob = await this.http.get(src, { responseType: 'blob' }).toPromise();
+			const videoBlob = await this.http.get(src, { headers, responseType: 'blob' }).toPromise();
 			const reader = new FileReader();
 			return new Promise((resolve, reject) => {
 				reader.onloadend = () => resolve(reader.result as string);
