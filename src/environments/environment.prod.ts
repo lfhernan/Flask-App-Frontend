c8c1e019a6e1d730e9ca50979e279d2a46bd0c70
Luis Hernandez <lfhernan@fiu.edu>
2020-11-01 23:49:47 -0500

    enabled prod enviromnet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+	production: true,
+	auth: {
+		domain: 'dev-f3yp6fj9.us.auth0.com',
+		clientId: '0KWFX79CPyrW2zCVJWZm8Lc8nz7FuhVj',
+		audience: "https://flask.api",
+		redirectUri: window.location.origin,
+	},
+	dev: {
+		serverUrl: "http://localhost:5000"
+	}
 };
